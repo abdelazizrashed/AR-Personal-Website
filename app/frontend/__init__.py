7969cdcebc0019016f193ead516d7888182c82e2
abdelazizrashed <abdelaziz.y.rashed@gmail.com>
2021-07-06 17:54:10 +0200

    Finish front-end navbar and layout template

--- app/frontend/__init__.py
+++ app/frontend/__init__.py
@@ -1,7 +1,29 @@
+import os
 from flask import Flask, render_template
+import jinja2
 
 app = Flask(__name__)
 
-@app.route('/')
+loader = jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), "templates"))
+
+enviroment = jinja2.Environment(loader=loader)
+
+
+@app.route("/")
 def home():
-    return render_template('pages/index.html')
+    nav = [
+        {"name": "Home", "url": "/"},
+        {"name": "Projects", "url": "/projects"},
+        {"name": "Services", "url": "/services"},
+        {"name": "Contact", "url": "/contact"},
+        {"name": "Resume", "url": "/resume"},
+        {"name": "Certificates", "url": "/certificates"},
+        {"name": "About", "url": "/about"},
+    ]
+    return render_template(
+        "pages/index.html",
+        nav=nav,
+        title="Abdelaziz Rashed Personal Website",
+        url = '/',
+        description="Abdelaziz Rashed is a software developer with wide skill set and experience in Web Development, Cross-platform App Development and Game Development",
+    )
